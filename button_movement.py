27291d1919be3241d1117ea6c99638b2770b528a
AAmir007-code <amiraslonov00@gmail.com>
2023-04-11 01:11:01 -0400

    initial code

---
+++ button_movement.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+app = QApplication(['Button movement'])
+app.setFont(QFont('Ubuntu', 25))
+win = QMainWindow()
+win.setGeometry(300, 300, 700, 700)
+def setBtn(btn, val, x, y, chx, chy):
+    btn.setText(val)
+    btn.move(x, y)
+    btn.setFont(QFont('Ubuntu', 50))
+    btn.adjustSize()
+    btn.clicked.connect(lambda: MainBtn.move(MainBtn.x()+chx,MainBtn.y()+chy))
+posX, posY, step = 250, 250, 5
+MainBtn = QPushButton(win)
+setBtn(MainBtn, '🐯', posX, posY, 0, 0)
+btn1 = QPushButton(win)
+setBtn(btn1, "←", 120, 600, -step, 0)
+btn2 = QPushButton(win)
+setBtn(btn2, "↓", 210, 600, 0, step)
+btn3 = QPushButton(win)
+setBtn(btn3, "→", 300, 600, step, 0)
+btn4 = QPushButton(win)
+setBtn(btn4, "↑", 210, 510, 0, -step)
+win.show()
+app.exec()
